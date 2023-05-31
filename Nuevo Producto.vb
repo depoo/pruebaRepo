3a16d7465431c5cb125e928eb3498983577f8f81
depoo <paul.fcb55@gmail.com>
2023-05-31 15:32:10 -0500

    Falta terminar codigo en la vista Nuevo producto

--- Nuevo Producto.vb
+++ Nuevo Producto.vb
@@ -1,27 +1,27 @@
-﻿Public Class Nuevo_Producto
+﻿Imports System.Reflection.Metadata
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Public Class Nuevo_Producto
     Dim idNuevoProducto, idProveedor As Integer
     Dim nombreProducto As String
 
     Public Sub cargarProveedor()
         Try
-            Using dbContext As New MiDbContext
-                Dim query = From N In dbContext.Proveedor
-                            Join A In dbContext.Actores On N.id_Actor Equals A.id_Actor
-                            Select New With {
-                                .idProveedor = N.id_Proveedor,
-                                .Nombre = A.NombreCompleto
-                                }
-
-                ComboBox1.DataSource = query.ToList()
-                ComboBox1.DisplayMember = "Nombre"
-                ComboBox1.SelectedValue = "idProveedor"
-
-            End Using
+            Dim dbContext As New MiDbContext()
+            Dim query = From N In dbContext.Proveedor
+                        Join A In dbContext.Actores On N.id_Actor Equals A.id_Actor
+                        Select New With {
+                    .idProveedor = N.id_Proveedor,
+                    .Nombre = A.NombreCompleto
+                }
+            Dim proveedores As IEnumerable(Of Object) = query.ToList()
+            ComboBox1.DataSource = proveedores
+            ComboBox1.DisplayMember = "Nombre"
+            ComboBox1.ValueMember = "idProveedor"
         Catch ex As Exception
             cajademensaje.Errordeobtencion()
         End Try
-
     End Sub
+
     Private Sub Nuevo_Producto_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
         cargarProveedor()
