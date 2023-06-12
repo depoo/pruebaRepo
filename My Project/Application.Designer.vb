e19e8a8c8d125f4e119add844d8d505ed50baadc
Paul Francis <paul.fcb55@gmail.com>
2023-06-11 19:03:35 -0500

    cambiando la vista de login como principal

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -31,7 +31,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = FormularioIngredientes1
+            Me.MainForm = FormularioLogin
         End Sub
     End Class
 End Namespace
